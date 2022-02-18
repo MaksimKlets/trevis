2ea62d01cb3f8359becc8f35c18729a979c4cae8
Maksym Klets <dalalsQ_123123123>
2022-02-18 15:41:36 +0200

    Change server.js file

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist/trevis-ci'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/trevis-ci/index.html'));
-});
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/trevis-ci'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/trevis-ci/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
